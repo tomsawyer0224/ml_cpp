81cc3a82238918bdf67e294ff6c185c6cd715099
tomsawyer0224 <130035084+tomsawyer0224@users.noreply.github.com>
2024-07-25 10:21:39 +0700

    Update neuralnet.hpp

--- acapulco_bits/neural_network/neuralnet.hpp
+++ acapulco_bits/neural_network/neuralnet.hpp
@@ -3,22 +3,6 @@ MLP::MLP(const std::vector<int>& layers):BaseModel::BaseModel(){
 	this->params_string["activation"] = "sigmoid";
 	//this->params_bool["early_stopping"] = true;
 	this->layers = layers;
-	
-	/*
-	int n_layers = layers.size();
-	this->coefs.set_size(n_layers - 1);
-	this->intercepts.set_size(n_layers - 1);
-	for(int i = 0; i < n_layers - 1; i++){
-		//arma::mat coef(layers[i+1], layers[i]);
-		arma::mat coef(layers[i+1], layers[i], arma::fill::randu);
-		coefs(i) = coef;
-		//arma::mat intercept(layers[i+1], 1);
-		//arma::mat intercept(layers[i+1], 1, arma::fill::randu);
-		arma::mat intercept(layers[i+1], 1, arma::fill::zeros);
-		intercepts(i) = intercept;
-	}
-	*/
-	//pkg.unpack(this->coefs, this->intercepts);
 }
 
 void MLP::init_coefs_inters(arma::field<arma::mat>& coefs, arma::field<arma::mat>& intercepts){
@@ -32,7 +16,6 @@ void MLP::init_coefs_inters(arma::field<arma::mat>& coefs, arma::field<arma::mat
 	this->coefs = coefs;
 	this->intercepts = intercepts;
 }
-
 
 void MLP::get_params(){
 	std::cout<<"layers: ";
@@ -84,21 +67,6 @@ void MLP::fit(const arma::mat& X_train, const arma::mat& y_train){
 	    this->set_params("layers", this->layers);
 	    this->fitted = true;
 	}
-	
-	/*
-	if(this->layers.front() != n_features || this->layers.back() != n_classes){
-		std::vector<int> layers = this->layers;
-		layers.front() = n_features;
-		layers.back() = n_classes;
-		this->set_params("layers", layers);
-	}
-	*/
-	/*
-	std::cout<<"get_params in fit method: "<<std::endl;
-	this->get_params();
-	std::cout<<std::endl;
-	return;
-	*/
 	arma::mat theta = pkg.unpack(this->coefs, this->intercepts);
 	if(this->params_string["optimizer"] == "none"){
 		SGD solver(this->params_double["learning_rate"]);
@@ -216,95 +184,3 @@ arma::field<arma::field<arma::mat>> MLP::back_prop(const arma::field<arma::mat>&
 	grads(1) = grad_intercepts;
 	return grads;
 }
-
-/*
-template <class OPT>
-void MLP::_fit_SGD(arma::mat& theta, const arma::mat& X, const arma::mat& y, OPT& solver){
-//X: (n_samples x n_features), y: (n_samples x n_classes)
-	//arma::uword Xy_n_rows = X.n_rows, X_n_cols = X.n_cols, y_n_cols = y.n_cols;
-	
-	if(this->params_bool["early_stopping"]){//early_stopping = true
-		if(this->params_int.count("n_iter_no_change") == 0){
-			this->params_int["n_iter_no_change"] = 10;
-		}
-		
-		arma::field<arma::mat> train_val = train_test_split(X, y, 0.3, 0);//test_size = 0.3, shuffle = 0
-		arma::mat X_train, X_val, y_train, y_val;
-		X_train = train_val(0); X_val = train_val(1); y_train = train_val(2); y_val = train_val(3);
-		
-		arma::uword X_train_n_rows = X_train.n_rows, X_train_n_cols = X_train.n_cols, y_train_n_cols = y_train.n_cols;
-		std::cout<<"X_train_n_rows: "<<X_train_n_rows<<std::endl;
-		std::cout<<"X_train_n_cols: "<<X_train_n_cols<<std::endl;
-		std::cout<<"y_train_n_cols: "<<y_train_n_cols<<std::endl;
-		X_train.brief_print("X_train:");
-		X_val.brief_print("X_val:");
-		y_train.brief_print("y_train:");
-		y_val.brief_print("y_val:");
-		//return;
-		double best_score = -1;
-		arma::mat best_coef(theta);
-
-		int no_change = 0;
-		for(int iter = 0; iter < this->params_int["max_iter"]; iter++){
-			if(no_change > this->params_int["n_iter_no_change"]){
-				break;
-			}
-			arma::uvec index = arma::randperm(X_train_n_rows);
-			for(arma::uword i = 0; i < X_train_n_rows; i++){
-				arma::mat Xi = X_train.submat(index[i],0,index[i],X_train_n_cols-1); 
-				arma::mat yi = y_train.submat(index[i],0,index[i],y_train_n_cols-1);
-				Xi.brief_print("Xi:");
-				yi.print("yi:");
-				
-				arma::mat grad = this->gradient(theta, Xi, yi);
-				solver.update(theta, grad);
-			}
-			//exit(1);
-			//check stopping criteria
-			arma::field<arma::field<arma::mat>> ci = pkg.pack(theta);
-			//pkg.reset();
-			this->coefs = ci(0); this->intercepts = ci(1);
-			
-			//convert y_val to y (1 column)
-			arma::mat y_val_one_col = one_hot_decoder(y_val);
-			double curr_score = this->score(X_val, y_val_one_col);
-			
-			if(curr_score > best_score + this->params_double["tol"]){
-				no_change = 0;
-				best_score = curr_score;
-				best_coef = theta;
-			}
-			else{
-				no_change++;
-			}
-		}
-		arma::field<arma::field<arma::mat>> ci_best = pkg.pack(best_coef);
-		this->coefs = ci_best(0); this->intercepts = ci_best(1);
-		return;
-	}
-	
-	
-	arma::uword Xy_n_rows = X.n_rows, X_n_cols = X.n_cols, y_n_cols = y.n_cols;
-	arma::mat theta_last(theta);
-	for(int iter = 0; iter < this->params_int["max_iter"]; iter++){
-		arma::uvec index = arma::randperm(Xy_n_rows);
-		for(arma::uword i = 0; i < Xy_n_rows; i++){
-			arma::mat Xi = X.submat(index[i],0,index[i],X_n_cols-1); 
-			arma::mat yi = y.submat(index[i],0,index[i],y_n_cols-1);
-			arma::mat grad = this->gradient(theta, Xi, yi);
-			solver.update(theta, grad);
-			//check stopping criteria
-			if(arma::norm(theta - theta_last, "fro") < this->params_double["tol"]){
-				this->set_params("coef", theta);
-				this->set_params("n_iters", iter+1);
-				return;
-			}
-		}
-	}
-	theta_last = theta;
-	this->set_params("coef", theta);
-	this->set_params("n_iters", params_int["max_iter"]);
-	return;
-}
-*/
-
